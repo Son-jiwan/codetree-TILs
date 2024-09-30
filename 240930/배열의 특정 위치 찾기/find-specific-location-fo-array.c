6d907cd7e3cd16d1174e24873843e6d4f8e45a9b
Son-jiwan <jwson0407@yonsei.ac.kr>
2024-09-30 22:03:38 +0900

    Title: 배열의 특정 위치 찾기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240930/배열의 특정 위치 찾기/find-specific-location-fo-array.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[10];
+    int a=0;
+    int b=0;
+    int count=0;
+    for(int i=0; i<10; i++){
+        scanf("%d", &arr[i]);
+    }
+    for(int i=0; i<10; i++){
+        if((i+1) % 2 ==0){
+            a+=arr[i];
+        }
+        if((i+1) % 3 ==0){
+            b+=arr[i];
+            count++;
+        }
+    }
+    double c = b/count;
+
+    printf("%d %.1f", a,c);
+    
+    return 0;
+}
