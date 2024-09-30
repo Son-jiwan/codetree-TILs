ff9cfa989e88062cd0a1ad56790e6681a0f30ce3
Son-jiwan <jwson0407@yonsei.ac.kr>
2024-09-30 19:22:22 +0900

    Title: 다양하게 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240930/다양하게 출력/print-in-variety.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 365;
+    double b = 3.1415926535;
+    printf("Total days in Year\n");
+    printf("%d\n",a);
+    printf("Circumference rate\n");
+    printf("%.10lf\n",b);
+    return 0;
+}
